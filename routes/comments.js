f8fdc09758a82152dbd158e92638cefce6c048ca
rolfilho <rolfilho@gmail.com>
2017-10-21 13:25:38 +0000

    landing now responsive

--- routes/comments.js
+++ routes/comments.js
@@ -41,6 +41,7 @@ router.post("/campgrounds/:id/comments", middleware.IsLoggedIn, function(req, re
                    comment.save();
                    campground.comments.push(comment);
                    campground.save();
+                   req.flash("succces", "Comment created!");
                    res.redirect("/campgrounds/" + campground._id);
                }
            });
